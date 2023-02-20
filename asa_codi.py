fbceb18f03ca2c7fc5be3f9213949522cc78a65e
Ada Salvador <ada.salvador@upc.edu>
2023-02-20 12:58:41 +0100

    change2

--- asa_codi.py
+++ asa_codi.py
@@ -65,6 +65,9 @@ plt.plot(t[0:Ls], x_r[0:Ls])                # Representació del senyal en funci
 plt.xlabel('t en segons')                 # Etiqueta eix temporal
 plt.title('5 periodes de la sinusoide')   # Títol del gràfic
 plt.show()                                # Visualització de l'objecte gràfic. 
+
+sd.play(x_r, fm)    
+
 N=5000                        # Dimensió de la transformada discreta
 X=fft(x_r[0 : Ls], N)           # Càlcul de la transformada de 5 períodes de la sinusoide
 k=np.arange(N)                        # Vector amb els valors 0≤  k<N
@@ -81,3 +84,44 @@ plt.show()
 
 #---------------------------------------------------------------------------------------
 #Exercici 3
+
+T= 2.5                               # Durada de T segons
+fm=8000                              # Freqüència de mostratge en Hz
+fx=440                               # Freqüència de la sinusoide
+A=4                                  # Amplitud de la sinusoide
+pi=np.pi                             # Valor del número pi
+L = int(fm * T)                      # Nombre de mostres del senyal digital
+Tm=1/fm                              # Període de mostratge
+t=Tm*np.arange(L)                    # Vector amb els valors de la variable temporal, de 0 a T
+x = A * np.cos(2 * pi * fx * t)      # Senyal sinusoidal
+sf.write('nom_fitxer.wav', x, fm)   # Escriptura del senyal a un fitxer en format wav
+Tx=1/fx                                   # Període del senyal
+Ls=int(fm*5*Tx)                           # Nombre de mostres corresponents a 5 períodes de la sinusoide
+
+plt.figure(0)                             # Nova figura
+plt.plot(t[0:Ls], x[0:Ls])                # Representació del senyal en funció del temps
+plt.xlabel('t en segons')                 # Etiqueta eix temporal
+plt.title('5 periodes de la sinusoide')   # Títol del gràfic
+plt.show()                                # Visualització de l'objecte gràfic. 
+
+sd.play(x, fm)    
+
+
+N=5000                        # Dimensió de la transformada discreta
+X=fft(x[0 : Ls], N)           # Càlcul de la transformada de 5 períodes de la sinusoide
+k=np.arange(N)                        # Vector amb els valors 0≤  k<N
+FK=k/N * fm
+plt.figure(1)                         # Nova figura
+plt.subplot(211)                      # Espai per representar el mòdul
+plt.plot(FK,abs(20*np.log(X/max(X))))  # Representació del mòdul de la transformada
+plt.title(f'Transformada del senyal de Ls={Ls} mostres amb DFT de N={N}')   # Etiqueta del títol
+plt.ylabel('|X[k]|')                  # Etiqueta de mòdul
+plt.subplot(212)                      # Espai per representar la fase
+plt.plot(FK,np.unwrap(np.angle(20*np.log(X/max(X)))))    # Representació de la fase de la transformad, desenroscada
+plt.xlabel('Index k')                 # Etiqueta de l'eix d'abscisses 
+plt.ylabel('$\phi_x[k]$')             # Etiqueta de la fase en Latex
+plt.show() 
+
+#--------------------------------------------------------------------------------------
+
+#Exercici 4
